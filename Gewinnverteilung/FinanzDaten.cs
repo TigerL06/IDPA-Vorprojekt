75b3723373e67f1934b66932c64fda6dc5ed4619
TigerL06 <110893121+TigerL06@users.noreply.github.com>
2024-09-05 07:23:09 +0200

    Update FinanzDaten.cs

--- Gewinnverteilung/FinanzDaten.cs
+++ Gewinnverteilung/FinanzDaten.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +6,25 @@ using System.Threading.Tasks;
 
 namespace Gewinnverteilung
 {
-    internal class FinanzDaten
+    public class FinanzDaten
     {
+        int jahresgewinn;
+        int aktien;
+        int partizipialkapital;
+        int gesetzlicheReserven;
+        int vortrag;
+        int dividende;
+        public FinanzDaten() { }
+
+        public void Daten(int gewinn, int aktie, int parti, int gesetz, int vort, int div)
+        {
+            jahresgewinn = gewinn;
+            aktien = aktie;
+            partizipialkapital = parti;
+            gesetzlicheReserven = gesetz;
+            vortrag = vort;
+            dividende = div;
+        }
+
     }
 }
