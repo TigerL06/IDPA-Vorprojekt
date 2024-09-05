95eb103015d2bbb71523086c74784753c8b71786
TigerL06 <110893121+TigerL06@users.noreply.github.com>
2024-09-05 07:22:22 +0200

    Update Program.cs

--- Gewinnverteilung/Program.cs
+++ Gewinnverteilung/Program.cs
@@ -3,4 +3,13 @@ using Gewinnverteilung;
 BenutzerInteraktion interaktion = new BenutzerInteraktion();
 FinanzDaten daten = new FinanzDaten();
 
+int gewinn = interaktion.Gewinn();
+int aktie = interaktion.Aktie();
+int partizipal= interaktion.Partizipal();
+int reserve = interaktion.Reserven();
+int vortrag = interaktion.Vortrag();
+int dividen = interaktion.Dividende();
+
+daten.Daten(gewinn, aktie, partizipal, reserve, vortrag, dividen);
+
 Berechnung verteilung = new Berechnung();
